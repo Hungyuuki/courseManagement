434a1614693a426a315a6632a782c3edb6abd55d
LDAP\pmhung2 <pmhung2@cmcglobal.vn>
2023-02-27 14:05:15 +0700

    CRUD OK

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-
-
-    var coursesAPI = 'http://localhost:3000/courses';
+var coursesAPI = 'http://localhost:3000/courses';
 
 function start() {
     getCourses(renderCourses)
@@ -29,7 +27,7 @@ function createCourse(data, callback) {
             'Content-Type': 'application/json'
         }
     }
-    fetch(coursesAPI, options)
+    fetch(coursesAPI, options) //fetch để tìm và nạp tài nguyên từ một nguồn nào đó
     .then(function(response){
         response.json();
     })
@@ -57,7 +55,9 @@ function handleDeleteCourse(id) {
     })
 }
 
-function updateCourse(id, data, callback) {
+
+//API Update
+function handleUpdateCourse(data, id, callback) {
     var options = {
         method: 'PUT',
         body: JSON.stringify(data),
@@ -73,44 +73,49 @@ function updateCourse(id, data, callback) {
 }
 
 
-function handleUpdateCourses(id) {
+function handleUpdateForm(id) {
         var buttonUpdate = document.querySelector('#create');
         buttonUpdate.innerHTML = 'Update';
         var course;
-        getCourses(function(course) {
-            course = courses.find((courseItem) => courseItem.id == id)
+        getCourses(function(courses) {
+            course = courses.find(function (courseItem)  {
+                return courseItem.id == id
+            })
         });
         var title = document.querySelector('input[name="title"]');
         var author = document.querySelector('input[name="author"]');
-        title.value = course.title;
-        author.value = course.author;
+        var description = document.querySelector('input[name="des"]');
+
         buttonUpdate.onclick = function() {
             var formData = {
                 title: title.value,
-                author: author.value
+                author: author.value,
+                description: description.value
             }
-            handleCreateForm(formData, id, function() {
+            handleUpdateCourse(formData, id, function() {
                 getCourses(renderCourses);
-                document.querySelector('#create').innerHTML = 'Create'
+                document.querySelector('#create').innerHTML = 'Update'
             })
         }
     }
 
 
-function renderCourses(courses) {//rend ra html
+function renderCourses(courses) {//rend ra html, đây chính là callback của hàm getCourse ban đầu
     var listCoursesBlock = document.querySelector('#list-courses');//lấy ra id list-course gán vào biến
-    var html = courses.map(function(course) //trả về một mảng mới
+    var htmls = courses.map(function(course) //trả về một mảng mới danh sách các course
     {
         return `
         <li class ="course-item-${course.id}">
-        <h4>${course.title}</h4>
-        <h4>${course.author}</h4>
+        <h4>Tên khóa học: ${course.title}</h4>
+        <h4>Giảng viên: ${course.author}</h4>
+        <p>Mô tả khóa học: ${course.description}</p>
         <button style = "background-color: beige" onclick="handleDeleteCourse(${course.id})">Delete</button>
-        <button style = "background-color: aqua" onclick="handleDeleteCourse(${course.id})">Update</button>
+        <button style = "background-color: aqua" onclick="handleUpdateForm(${course.id})">Update</button>
         </li>
         `
     })
-    listCoursesBlock.innerHTML = html.join('') //join các khóa lại với nhau
+    console.log(courses)
+    listCoursesBlock.innerHTML = htmls.join('')//Nếu ko có cặp '' thì sẽ mặc định nối với nhau bằng dấu phẩy
 }
 
 
@@ -120,9 +125,11 @@ function handleCreateForm() {//viết logic tạo mới dl
     createBtn.onclick = function () {
         var title = document.querySelector('input[name="title"]').value;
         var author = document.querySelector('input[name="author"]').value;
+        var description = document.querySelector('input[name="des"]').value;
         var formData = {
             title: title,
-            author: author
+            author: author,
+            description: description
         }
         createCourse(formData, function() {//sau khi create xong thì ta gọi luôn getcourses để render ra html luôn, ko cần tải lại trang
             getCourses(renderCourses)
